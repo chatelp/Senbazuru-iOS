e1d34edf7e742cc4efffe4c434ef7b421fe209f6
Pierre Chatel <pierre@iMac.local>
2014-09-11 09:26:33 +0200

    Début implémentation IHM iPad

---
+++ Senbazuru/iPadMainController.h
@@ -0,0 +1,13 @@
+//
+//  iPadMainController.h
+//  Senbazuru
+//
+//  Created by Pierre Chatel on 11/09/14.
+//  Copyright (c) 2014 Pierre Chatel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iPadMainController : UISplitViewController
+
+@end
