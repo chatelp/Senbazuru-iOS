5597471af5f72cda659f165926c78542fa29b08c
Pierre Chatel <pierre@iMac.local>
2015-03-19 18:35:38 +0100

    maj code swift

--- Senbazuru/Origami.swift
+++ Senbazuru/Origami.swift
@@ -7,3 +7,77 @@
 //
 
 import Foundation
+import UIKit
+
+class SwiftOrigami {
+    private var wrappedItem : MWFeedItem;
+    
+    init(wrappedItem: MWFeedItem) {
+        self.wrappedItem = wrappedItem;
+    }
+
+    // MARK: -
+    // MARK: Wrapper accessors for feed item properties
+    
+    var date : NSDate {
+        get {
+            return wrappedItem.date
+        }
+    }
+    
+    var title : String {
+        get {
+            if wrappedItem.title != nil {
+                return wrappedItem.title.stringByConvertingHTMLToPlainText()
+            } else {
+                return "[No Title]"
+            }
+        }
+    }
+    
+    var summary : String {
+        get {
+            if wrappedItem.summary != nil {
+                return wrappedItem.summary.stringByConvertingHTMLToPlainText()
+            } else {
+                return "[No Summary]"
+            }
+        }
+    }
+
+    var content : String {
+        get {
+            if wrappedItem.content != nil {
+                return wrappedItem.content.stringByConvertingHTMLToPlainText()
+            } else {
+                return "[No Content]"
+            }
+        }
+    }
+    
+    // MARK: -
+    // MARK: Accessor for dedicated origami properties
+    
+    var summaryPlainText : String {
+        get {
+            if wrappedItem.summary != nil {
+                return wrappedItem.summary.stringByConvertingHTMLToPlainText()
+            } else {
+                return "[No Summary]"
+            }
+        }
+    }
+    
+    var contentPlainText : String {
+        get {
+            if wrappedItem.content != nil {
+                return wrappedItem.content.stringByConvertingHTMLToPlainText()
+            } else {
+                return "[No Content]"
+            }
+        }
+    }
+
+    
+    
+}
--- Senbazuru/Senbazuru-Bridging-Header.h
+++ Senbazuru/Senbazuru-Bridging-Header.h
@@ -2,3 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+@import UIKit;
+#import "MWFeedItem.h"
+#import "NSString+HTML.h"
+
